5d737d4a6448b3e77d2046c54378a24b8f6bf114
yagmurozkok <43780900+yagmurozkok@users.noreply.github.com>
2020-09-14 16:10:33 +0300

    Add files via upload

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,7 @@
+import 'package:e_masa/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:e_masa/screens/authenticate/authenticate.dart';
+import 'package:flutter/material.dart';
+import 'package:e_masa/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
